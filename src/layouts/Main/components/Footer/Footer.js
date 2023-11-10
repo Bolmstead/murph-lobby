143b7accd0a47db33462028733b04ff27d407c2a
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2023-11-09 18:58:56 -0700

    footer edit

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
@@ -12,13 +11,6 @@ const Footer = () => {
           color="text.secondary"
           gutterBottom
         >
-          Site built by{" "}
-          <a
-            style={{ color: "inherit" }}
-            href="https://www.berkleyolmstead.com"
-          >
-            Olmstead Tech Services Inc.
-          </a>
           <Typography
             align={"center"}
             variant={"subtitle2"}
