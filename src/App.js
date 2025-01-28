488c90a68f7cd41986263692bbf3509e774a005a
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2025-01-28 08:41:54 -0700

    disabled site. blank page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
-import Page from './components/Page';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import Page from "./components/Page";
 
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import 'react-image-lightbox/style.css';
-import 'aos/dist/aos.css';
-import './app.css'
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "react-image-lightbox/style.css";
+import "aos/dist/aos.css";
+import "./app.css";
 
 const App = () => {
+  return <div></div>;
   return (
     <Page>
       <BrowserRouter>
